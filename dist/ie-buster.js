728959e9280bfd28952f9a9481993cc873a11d1e
Qrac <qrac.jp@gmail.com>
2021-02-19 19:51:20 +0900

    Remove class

--- dist/ie-buster.js
+++ dist/ie-buster.js
@@ -2,82 +2,52 @@
 this['ie-buster'] = (function () {
   'use strict';
 
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
+  var ieBuster = function ieBuster(options) {
+    var defaultOptions = {
+      mainText: "ご利用のインターネットブラウザは推奨環境ではありません。Webサイトの動作が保証できませんので、最新の Google Chrome をご利用ください。",
+      linkText: "ダウンロードページへ",
+      linkUrl: "https://www.google.com/chrome/"
+    };
+    options = options || {};
+    options.mainText = options.mainText || defaultOptions.mainText;
+    options.linkText = options.linkText || defaultOptions.linkText;
+    options.linkUrl = options.linkUrl || defaultOptions.linkUrl;
 
-  function _defineProperties(target, props) {
-    for (var i = 0; i < props.length; i++) {
-      var descriptor = props[i];
-      descriptor.enumerable = descriptor.enumerable || false;
-      descriptor.configurable = true;
-      if ("value" in descriptor) descriptor.writable = true;
-      Object.defineProperty(target, descriptor.key, descriptor);
-    }
-  }
-
-  function _createClass(Constructor, protoProps, staticProps) {
-    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
-    if (staticProps) _defineProperties(Constructor, staticProps);
-    return Constructor;
-  }
-
-  var IEBuster = /*#__PURE__*/function () {
-    function IEBuster(options) {
-      _classCallCheck(this, IEBuster);
-
-      options = {};
-      this.mainText = options.mainText || "ご利用のインターネットブラウザは推奨環境ではありません。Webサイトの動作が保証できませんので、最新の Google Chrome をご利用ください。";
-      this.linkText = options.linkText || "ダウンロードページへ";
-      this.linkUrl = options.linkUrl || "https://www.google.com/chrome/";
-    }
-
-    _createClass(IEBuster, [{
-      key: "init",
-      value: function init() {
-        if (this.isIE()) {
-          this.reset();
-          this.create();
-        }
+    var isIE = function isIE() {
+      if (!!window.MSInputMethodContext && !!document.documentMode || false) {
+        return true;
       }
-    }, {
-      key: "isIE",
-      value: function isIE() {
-        if (!!window.MSInputMethodContext && !!document.documentMode || false) {
-          return true;
-        }
-      }
-    }, {
-      key: "reset",
-      value: function reset() {
-        var resetItem = document.getElementById("ie-buster-app");
+    };
 
-        if (resetItem) {
-          resetItem.parentNode.removeChild(resetItem);
-        }
-      }
-    }, {
-      key: "create",
-      value: function create() {
-        var ieBusterTarget = document.getElementsByTagName("body")[0];
-        var ieBusterApp = document.createElement("div");
-        ieBusterApp.id = "ie-buster-app";
-        ieBusterApp.setAttribute("style", "position: fixed; top: 0; left: 0; width: 100%; padding: 16px; box-sizing: border-box; z-index: 999999;");
-        ieBusterApp.innerHTML = "<div style=\"width: 100%; max-width:866px; margin: 0 auto; padding: 16px 20px; background-color: #fff; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 5px 0px; box-sizing: border-box; font-family: SegoeUI, Meiryo, sans-serif;\">" + "<p style=\"display: block; float: left; width: 100%; max-width: 664px; margin: 0; color: #000; font-size: 14px; font-weight: 400; line-height: 1.5;\">" + this.mainText + "</p>" + "<a style=\"display: block; float: right; height: 36px; width: 154px; padding: 0 16px; background-color: rgb(0, 120, 212); box-sizing: border-box; color: #fff; font-size: 12px; font-weight: 400; line-height: 36px; text-align: center; text-decoration: none; white-space: nowrap;\" href=\"" + this.linkUrl + "\" target=\"_blank\" rel=\"noopener noreferrer\">" + this.linkText + "</a>" + "<div style=\"clear: both;\"></div>" + "</div>";
-        ieBusterTarget.appendChild(ieBusterApp);
-      }
-    }]);
+    var reset = function reset() {
+      var target = document.getElementById("ie-buster-app");
+      target && target.remove();
+    };
 
-    return IEBuster;
-  }();
+    var create = function create() {
+      var body = document.getElementsByTagName("body")[0];
+      var app = document.createElement("div");
+      app.id = "ie-buster-app";
+      app.setAttribute("style", "position: fixed; top: 0; left: 0; width: 100%; padding: 16px; box-sizing: border-box; z-index: 999999;");
+      app.innerHTML = "<div style=\"width: 100%; max-width:866px; margin: 0 auto; padding: 16px 20px; background-color: #fff; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 5px 0px; box-sizing: border-box; font-family: SegoeUI, Meiryo, sans-serif;\">" + "<p style=\"display: block; float: left; width: 100%; max-width: 664px; margin: 0; color: #000; font-size: 14px; font-weight: 400; line-height: 1.5;\">" + options.mainText + "</p>" + "<a style=\"display: block; float: right; height: 36px; width: 154px; padding: 0 16px; background-color: rgb(0, 120, 212); box-sizing: border-box; color: #fff; font-size: 12px; font-weight: 400; line-height: 36px; text-align: center; text-decoration: none; white-space: nowrap;\" href=\"" + options.linkUrl + "\" target=\"_blank\" rel=\"noopener noreferrer\">" + options.linkText + "</a>" + "<div style=\"clear: both;\"></div>" + "</div>";
+      body.appendChild(app);
+    };
+
+    var init = function init() {
+      if (isIE()) {
+        reset();
+        create();
+      }
+    };
+
+    return init();
+  };
+  window.ieBuster = ieBuster;
 
   window.onload = function () {
-    var ieBuster = new IEBuster({});
-    ieBuster.init();
+    ieBuster();
   };
 
-  return IEBuster;
+  return ieBuster;
 
 }());
--- src/index.js
+++ src/index.js
@@ -1,63 +1,66 @@
-class IEBuster {
-  constructor(options) {
-    options = {}
-    this.mainText =
-      options.mainText ||
-      "ご利用のインターネットブラウザは推奨環境ではありません。Webサイトの動作が保証できませんので、最新の Google Chrome をご利用ください。"
-    this.linkText = options.linkText || "ダウンロードページへ"
-    this.linkUrl = options.linkUrl || "https://www.google.com/chrome/"
+const ieBuster = (options) => {
+  const defaultOptions = {
+    mainText:
+      "ご利用のインターネットブラウザは推奨環境ではありません。Webサイトの動作が保証できませんので、最新の Google Chrome をご利用ください。",
+    linkText: "ダウンロードページへ",
+    linkUrl: "https://www.google.com/chrome/",
   }
 
-  init() {
-    if (this.isIE()) {
-      this.reset()
-      this.create()
-    }
-  }
+  options = options || {}
+  options.mainText = options.mainText || defaultOptions.mainText
+  options.linkText = options.linkText || defaultOptions.linkText
+  options.linkUrl = options.linkUrl || defaultOptions.linkUrl
 
-  isIE() {
+  const isIE = () => {
     if ((!!window.MSInputMethodContext && !!document.documentMode) || false) {
       return true
     }
   }
 
-  reset() {
-    const resetItem = document.getElementById("ie-buster-app")
-    if (resetItem) {
-      resetItem.parentNode.removeChild(resetItem)
-    }
+  const reset = () => {
+    const target = document.getElementById("ie-buster-app")
+    target && target.remove()
   }
 
-  create() {
-    const ieBusterTarget = document.getElementsByTagName("body")[0]
-    const ieBusterApp = document.createElement("div")
+  const create = () => {
+    const body = document.getElementsByTagName("body")[0]
+    const app = document.createElement("div")
 
-    ieBusterApp.id = "ie-buster-app"
-    ieBusterApp.setAttribute(
+    app.id = "ie-buster-app"
+    app.setAttribute(
       "style",
       `position: fixed; top: 0; left: 0; width: 100%; padding: 16px; box-sizing: border-box; z-index: 999999;`
     )
 
-    ieBusterApp.innerHTML =
+    app.innerHTML =
       `<div style="width: 100%; max-width:866px; margin: 0 auto; padding: 16px 20px; background-color: #fff; box-shadow: rgba(0, 0, 0, 0.4) 0px 0px 5px 0px; box-sizing: border-box; font-family: SegoeUI, Meiryo, sans-serif;">` +
       `<p style="display: block; float: left; width: 100%; max-width: 664px; margin: 0; color: #000; font-size: 14px; font-weight: 400; line-height: 1.5;">` +
-      this.mainText +
+      options.mainText +
       `</p>` +
       `<a style="display: block; float: right; height: 36px; width: 154px; padding: 0 16px; background-color: rgb(0, 120, 212); box-sizing: border-box; color: #fff; font-size: 12px; font-weight: 400; line-height: 36px; text-align: center; text-decoration: none; white-space: nowrap;" href="` +
-      this.linkUrl +
+      options.linkUrl +
       `" target="_blank" rel="noopener noreferrer">` +
-      this.linkText +
+      options.linkText +
       `</a>` +
       `<div style="clear: both;"></div>` +
       `</div>`
 
-    ieBusterTarget.appendChild(ieBusterApp)
+    body.appendChild(app)
   }
+
+  const init = () => {
+    if (isIE()) {
+      reset()
+      create()
+    }
+  }
+
+  return init()
 }
 
-export default IEBuster
+export default ieBuster
+window.ieBuster = ieBuster
 
 window.onload = () => {
-  const ieBuster = new IEBuster({})
-  ieBuster.init()
+  ieBuster()
 }
