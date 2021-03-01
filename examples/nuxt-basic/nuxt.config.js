6305a06e4af76d17f38f39807336d628a96b1445
Qrac <qrac.jp@gmail.com>
2021-03-01 17:21:08 +0900

    Add a basic example of nuxt

---
+++ examples/nuxt-basic/nuxt.config.js
@@ -0,0 +1,45 @@
+export default {
+  // Target: https://go.nuxtjs.dev/config-target
+  target: "static",
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: "nuxt-basic",
+    htmlAttrs: {
+      lang: "en"
+    },
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: ["plugins/useIeBuster.js"],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+    // https://go.nuxtjs.dev/eslint
+    //'@nuxtjs/eslint-module'
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    //'@nuxtjs/axios'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  //axios: {},
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+}
---
+++ examples/nuxt-basic/plugins/useIeBuster.js
@@ -0,0 +1,3 @@
+import ieBuster from "ie-buster"
+
+process.browser && ieBuster.init()
